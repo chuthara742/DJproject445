b64d234062ee1636632bdd3fa1b64be04cb65f5f
chutharat742 <chutharat.pr@rmuti.ac.th>
2023-01-15 14:06:33 +0700

    Initial commit

---
+++ ProfileApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.http import HttpResponse
+from ProfileApp import views
+
+urlpatterns =[
+
+    # path('info',views.info, name="info"),
+    #
+    # path('edu',views.edu, name = "edu"),
+    #
+    # path('inter',views.inter, name = "inter"),
+    #
+    # path('myidol',views.myidol, name = "myidol"),
+    #
+    # path('manu',views.manu, name = "manu"),
+    # path('info', views.info, name='info'),
+
+]
---
+++ ProfileApp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+#Create your views here.
+ # def info(request):
+ #     return render(request,'infoSTD.html')
+ #
+ # def edu(request):
+ #     return render(request,'eduSTD.html')
+ #
+ # def inter (request):
+ #     return render(request,'inter.html')
+ #
+ # def myidol (request):
+ #     return render(request,'myidol.html')
+ #
+ # def manu (request):
+ #     return render(request,'manu.html')
+ #
+ # def info(request):
+ #      return render(request,'infoSTD.html')
